02d84608f5c152cbec7368904b75ee6910133a8e
Ryan Seo <dmseo@dataize.io>
2021-10-22 11:04:43 +0900

    First Commit

---
+++ src/context/BlogContext.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+    // type === 'add_blogpost' || 'edit_blogpost' || 'delete'
+    switch(action.type) {
+        case 'add_blogpost':
+            return [ ...state, { title: `Blog Post #${state.length + 1}` }];
+        default:
+            return state;
+    }
+}
+
+export const BlogProvider = ({ children }) => {
+
+    const [ blogPosts, dispatch ] = useReducer(blogReducer, []);
+
+    const addBlogPost = () => {
+        dispatch({ type: 'add_blogpost' });
+    }
+    
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+        { children }
+    </BlogContext.Provider>
+}
+
+export default BlogContext;
